3dcb64668174528be573f030861d039bacf24bfc
Krish Lalwani <35841954+KrishLalwani@users.noreply.github.com>
2018-10-14 18:25:39 +0530

    Made code for LECANDY

--- codechef/LECANDY.cpp
+++ codechef/LECANDY.cpp
@@ -1,35 +1,22 @@
-#include <bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
-
-#define INF (int)1e9
-#define EPS 1e-9
-#define REP(i, a, b) for(int i = a; i < b; ++i)
-#define PB push_back
-#define MP make_pair
-
-typedef long long ll;
-typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<string> VS;
-typedef vector<PII> VII;
-
-const float PI = 3.1415926535897932384626433832795;
-const int MOD = 1000000007;
-
 int main()
-{	
-	int t,n,c;
-	cin >> t;
-	while(t--){
-		cin >> n >> c;
-		ll a[n],sum = 0;
-		REP(i,0,n){
-			cin >> a[i];
-			sum += a[i];
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n,c,i,sum=0,temp;
+		cin>>n>>c;
+		for(i=0;i<n;i++)
+		{
+			cin>>temp;
+			sum=sum+temp;
 		}
-		if(c >= sum)cout << "Yes" << endl;
-		else cout << "No" << endl;
+		if(sum<=c)
+			cout<<"Yes\n";
+		else
+			cout<<"No\n";
 	}
-	return 0;
 }
+
